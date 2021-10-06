d1f4e3aa62637485acaa34390d6fcebcbbee0e92
Linerre <leon.errelin@outlook.com>
2021-10-06 15:45:50 +0800

    more fixes

--- themes/fix-terminal-color-theme.el
+++ themes/fix-terminal-color-theme.el
@@ -8,6 +8,12 @@
    'fix-terminal-color
    '(company-tooltip ((t (:background "#fbf1c7" :foreground "#3c383d"))))
    '(mode-line ((t (:background "#bdae93 " :foreground "#3c383d"))))
+   '(font-lock-comment-face ((t (:foreground "#a89984"))))
+   '(font-lock-function-name-face ((t (:foreground "#458588"))))
+   '(font-lock-keyword-face ((t (:foreground "#cc241d"))))
+   '(font-lock-constant-face ((t (:foreground "#381a8a"))))
+   '(font-lock-string-face ((t (:foreground "#98971a"))))
+   '(font-lock-builtin-face ((t (:foreground "#b16286"))))
 )
 
 (and load-file-name
