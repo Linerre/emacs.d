db23974d613dc258e279f0cc493497536866dcfc
Linerre <leon.errelin@outlook.com>
2021-10-08 21:32:40 +0800

    disable code related to package.el

--- elisp/init-package.el
+++ elisp/init-package.el
@@ -5,10 +5,10 @@
 ;; Commentary:
 ;; Swithed to straight.el. This file exits solely for searching through melpa, see
 ;; https://github.crookster.org/switching-to-straight.el-from-emacs-26-builtin-package.el/#step-2-convertremove-packageel-stuff
-(require 'package)
-(setq package-archives '(("gnu"   . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/gnu/")
-                         ("melpa" . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/melpa/")
-                         ("org"   . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/org/")))
+;;(require 'package)
+;;(setq package-archives '(("gnu"   . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/gnu/")
+;;                         ("melpa" . ;; "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/melpa/")
+;                         ("org"   . ;;"https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/org/")))
 
 (straight-use-package
  '(exec-path-from-shell
