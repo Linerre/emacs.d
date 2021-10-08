698260ba5ac2ca0546176aec45461c26dad04240
Linerre <leon.errelin@outlook.com>
2021-10-08 21:37:51 +0800

    move a pkg to init-straight

--- elisp/init-straight.el
+++ elisp/init-straight.el
@@ -4,7 +4,7 @@
 
 
 (setq straight-vc-git-default-clone-depth 1)
- 
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -17,5 +17,11 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+
+(straight-use-package
+ '(exec-path-from-shell
+   :type git :host github :repo "purcell/exec-path-from-shell"))
+ (when (memq window-system '(pc ns x))
+   (exec-path-from-shell-initialize))
 
 (provide 'init-straight)
