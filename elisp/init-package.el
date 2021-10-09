344ef6219159804587a7056710b0fe7de4c135fb
Linerre <leon.errelin@outlook.com>
2021-10-10 01:00:25 +0800

    install pakcages with package.el

--- elisp/init-package.el
+++ elisp/init-package.el
@@ -1,16 +1,47 @@
 ;;; -*- lexical-binding: t -*-
-;;; ------------------ MIRRORS, REPO, PKGS  ------------------
-
-
 ;; Commentary:
-;; Swithed to straight.el. This file exits solely for searching through melpa, see
-;; https://github.crookster.org/switching-to-straight.el-from-emacs-26-builtin-package.el/#step-2-convertremove-packageel-stuff
+;; Code:
 
-;; This code somwhow cause Emacs to rebuild every pkgs when it next starts up. Do not require this feature file in any other init files for now
+;; May need this
+;; (setq package-check-signature nil)
+
 (require 'package)
 (setq package-archives '(("gnu"   . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/gnu/")
                          ("melpa" .  "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/melpa/")
                          ("org"   . "https://mirrors.sjtug.sjtu.edu.cn/emacs-elpa/org/")))
+
+;; Initialize package.el unless this already done
+(unless (bound-and-true-p package--initialized)
+  (package-initialize))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(defvar packages-list
+  '(company
+    eglot
+    emmet-mode
+    flycheck
+
+    highlight-defined
+
+    jinja2-mode
+
+    kaolin-themes
+    magit
+    markdown-mode
+    meow
+    org-roam
+    ob-restclient
+    selectrum
+    smartparens
+    yasnippet
+    which-key))
+
+(dolist (p packages-list)
+  (unless (package-installed-p p)
+    (package-install p)))
+
 
 
 (provide 'init-package)
--- init.el
+++ init.el
@@ -1,10 +1,20 @@
-;;; early-init.el -*- lexical-binding: t -*-
+;;;  -*- lexical-binding: t -*-
 
+;;; Commentary:
 ;;; Modularized initialization
 ;;; Author: Errelin
 ;;; Last Change: Sat Aug 21 23:41:36 2021
+;;; Modules stored in the "elisp" directory
 
+;;; Code:
+(add-to-list 'load-path (expand-file-name "elisp" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "themes" user-emacs-directory))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+
+(require 'init-options)
 (require 'init-utils)
+(require 'init-packages)
 (require 'init-modal-qwerty)
 (require 'init-edit)
 (require 'init-completion)
