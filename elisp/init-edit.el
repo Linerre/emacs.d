577a2c37b846a09078fbf754f32c067698b28939
Linerre <leon.errelin@outlook.com>
2021-10-10 16:13:15 +0000

    fix syntax error

--- elisp/init-edit.el
+++ elisp/init-edit.el
@@ -38,7 +38,7 @@
 ;; it's good but when I switch between desktops it inserts
 ;; parens weirdly ... ONLY appears in TUI
 ;; due to "scroll alternate screen" option in terminal.app
-(when (or (display-graphic-p *is-linux*))
+(when (or (display-graphic-p) *is-linux*)
     (require 'init-smartparens))
 
 (provide 'init-edit)
