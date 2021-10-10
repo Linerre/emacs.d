612c6e601ca38216950b3596bf247b6efaa5a6ce
Linerre <leon.errelin@outlook.com>
2021-10-10 16:10:03 +0000

    clean theme file and fix font issue

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -1,26 +1,28 @@
 ;;; -*- lexical-binding: t -*-
-;;; ----------------------- THEME ----------------------
+;;; THEME
 
 
 (require 'kaolin-themes)
 (load-theme 'kaolin-light t)
 
-;; Disable tool bar, and scroll bar in GUI
-;; Set font as well
-;; Use display-graphic-p to detect gui(t) or tui(nil, text-only terminals, even running on DE)
-;; Use window-system to detect OS type: nil->char-only terminals; w32->windows; ns->macOS (deprecated according to GNU Emacs Manual)
+;; load the cutomized theme here
+;(load-theme 'customized-theme)
+
 (when (display-graphic-p)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
   (menu-bar-mode -1)
-  ;;(load-theme 'kaolin-light t)
-  (add-to-list 'default-frame-alist
-               '(font . "Courier-18")))
+  (cond (*is-mac*
+         (add-to-list 'default-frame-alist
+                      '(font . "Courier-18")))
+        (*is-linux*
+         (add-to-list 'default-frame-alist
+                      '(font . "Courier New-16:bold")))))
 
 ;; use terminal theme/font in TUI with minor fixes
 (when (not (display-graphic-p))
-
   (menu-bar-mode -1))
 
 (provide 'init-theme)
+
 ;;; theme ends here
