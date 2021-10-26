ae2d53f86328a541b5addfa4e8fec0c8a2b07b51
Linerre <leon.errelin@outlook.com>
2021-10-26 22:30:29 +0800

    clean up work

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(cdlatex which-key smartparens selectrum org-roam ob-restclient nix-mode meow markdown-mode magit latex-preview-pane kaolin-themes json-mode js2-mode jinja2-mode highlight-defined flycheck-clj-kondo exec-path-from-shell emmet-mode eglot company clj-refactor auctex-lua auctex-latexmk)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
