6ff169bfd207288917abf62c370ff4cf0f602592
Linerre <leon.errelin@outlook.com>
2021-10-28 21:26:18 +0800

    adopt DogLooksGood modeline to fix right-align problem

---
+++ elisp/init-modeline-dog.el
@@ -0,0 +1,31 @@
+;; Mode Line  -*- lexical-binding: t; -*-
+
+(setq-default header-line-format nil)
+
+(defun +format-mode-line ()
+  (let* ((lhs '((:eval (meow-indicator))
+                "[%2l:%2c]"
+                " "
+                "("
+                (-3 "%p")
+                "/%I)"
+                (vc-mode vc-mode)
+                (:eval (when (bound-and-true-p flycheck-mode) flycheck-mode-line))
+                (:eval (when (bound-and-true-p flymake-mode)
+                         flymake-mode-line-format))))
+         (rhs '("%b"
+                " ⊆ "
+                (:eval mode-name)
+                ))
+         (ww (window-width))
+         (lhs-str (format-mode-line lhs))
+         (rhs-str (format-mode-line rhs))
+         (rhs-w (string-width rhs-str)))
+    (format "%s%s%s"
+            lhs-str
+            (propertize " " 'display `((space :align-to (- (+ right right-fringe right-margin) (+ 1 ,rhs-w)))))
+            rhs-str)))
+
+(setq-default mode-line-format '((:eval (+format-mode-line))))
+
+(provide 'init-modeline-dog)
