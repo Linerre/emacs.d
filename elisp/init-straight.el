6202c0d642230c6247ac997dbe19987c7905a3d6
Linerre <leon.errelin@outlook.com>
2021-10-29 21:39:48 +0800

    only use exec-path-from-shell on macOD

--- elisp/init-straight.el
+++ elisp/init-straight.el
@@ -20,7 +20,7 @@
 (straight-use-package
  '(exec-path-from-shell
    :type git :host github :repo "purcell/exec-path-from-shell"))
- (when (memq window-system '(pc ns x))
+ (when (and (memq window-system '(ns)) (display-graphic-p))
    (exec-path-from-shell-initialize))
 
 (provide 'init-straight)
