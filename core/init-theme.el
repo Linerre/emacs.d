8ea1852e1624c66616dcd005d143e28fabb46f32
Linerre <leon.errelin@outlook.com>
2021-11-01 21:05:57 +0800

    use and config dired-sidebar

--- core/init-theme.el
+++ core/init-theme.el
@@ -1,6 +1,8 @@
 ;;; -*- lexical-binding: t -*-
 
 (straight-use-package 'kaolin-themes)
+(straight-use-package 'emacs-neotree)
+(straight-use-package 'dired-sidebar)
 
 (require 'kaolin-themes)
 (load-theme 'kaolin-light t)
@@ -9,6 +11,7 @@
 (require 'roam-research-default-theme)
 (load-theme 'roam-research-default t)
 
+;; font
 (when (display-graphic-p)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
@@ -23,6 +26,17 @@
 ;; use terminal theme/font in TUI with minor fixes
 (when (not (display-graphic-p))
   (menu-bar-mode -1))
+
+;; tree
+(autoload
+  #'dired-sidebar-toggle-sidebar "dired-sidebar" nil t)
+(global-set-key (kbd "C-v") #'dired-sidebar-toggle-sidebar)
+
+(with-eval-after-load "dired-sidebar"
+  (add-hook 'dired-sidebar-mode-hook 'hl-line-mode)
+  (setq dired-sidebar-theme 'ascii
+        dired-sidebar-use-custome-font t
+        dired-sidebar-face '(:weight bold)))
 
 (provide 'init-theme)
 
