3540c217178f17a8f929dc3ad02987f3b8823bcd
Linerre <leon.errelin@outlook.com>
2021-11-07 23:58:15 +0800

    reduce redaundant conf

--- langs/lang-nix.el
+++ langs/lang-nix.el
@@ -1,5 +1,5 @@
 ;;; -*- lexical-binding: t -*-
 
 (straight-use-package 'nix-mode)
-(add-to-list 'magic-mode-alist '("{" . nix-mode))
+;(add-to-list 'magic-mode-alist '("{" . nix-mode))
 (provide 'lang-nix)
