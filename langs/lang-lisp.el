c3730d3ea7f311e2f179df42452554582e4fcf8c
Linerre <leon.errelin@outlook.com>
2021-12-12 16:25:42 +0800

    Move hooks inside with-eval-after-load

--- langs/lang-lisp.el
+++ langs/lang-lisp.el
@@ -2,6 +2,12 @@
 
 (straight-use-package 'highlight-defined)
 
-(add-hook 'emacs-lisp-mode-hook 'highlight-defined-mode)
+;; elisp
+(with-eval-after-load "elisp-mode"
+  (add-hook 'emacs-lisp-mode-hook #'highlight-defined-mode)
+  (add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
+  (add-hook 'lisp-interaction-mode-hook #'eldoc-mode))
 
 (provide 'lang-lisp)
+
+;; lang-list ends here
