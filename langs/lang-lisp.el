c97e88442f03b953987b375f22ea9c38323da14f
Linerre <leon.errelin@outlook.com>
2021-12-13 10:13:24 +0800

    Check parens after save
    Usually this is not necessary since smartparens alone does well;
    but parens are especially many and important in Lisps

--- langs/lang-lisp.el
+++ langs/lang-lisp.el
@@ -2,10 +2,14 @@
 
 (straight-use-package 'highlight-defined)
 
+(autoload #'highlight-defined-mode "highlight-defined")
+
 ;; elisp
 (with-eval-after-load "elisp-mode"
   (add-hook 'emacs-lisp-mode-hook #'highlight-defined-mode)
   (add-hook 'emacs-lisp-mode-hook #'eldoc-mode)
+  (add-hook 'emacs-lisp-mode-hook
+            lambda () (add-hook 'after-save-hook #'check-parens))
   (add-hook 'lisp-interaction-mode-hook #'eldoc-mode))
 
 (provide 'lang-lisp)
