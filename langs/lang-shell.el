0d5a65fa13a1ebed08b470139f161b404f07a68b
Linerre <leon.errelin@outlook.com>
2021-12-26 18:24:23 +0800

    Hook sh-mode to bash_profile

--- langs/lang-shell.el
+++ langs/lang-shell.el
@@ -6,9 +6,14 @@
 ;;; Code:
 
 ;; shell-script-mode
-(add-to-list 'auto-mode-alist '("\\.sh\\'\\|\\.?[bz]a?shrc" . sh-mode))
+(add-to-list 'auto-mode-alist '("bash_profile\\'\\|[bz]a?shrc\\'" . sh-mode))
 
 ;; (with-eval-after-load "sh-script"
+;; vars can be set:
+;; sh-basic-offset -- defaults to 4
+;; sh-indentation -- defaults to 4 -- how many spaces to put in a deeper level
+;; sh-indent-for-case-label
+;; sh-indent-for-case-alt
 ;;   (message "Shell script mode"))
 
 (provide 'lang-shell)
