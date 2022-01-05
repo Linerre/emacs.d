c7331a8d9d75702193478f8c02de36c2561d18a3
Linerre <leon.errelin@outlook.com>
2022-01-05 21:23:54 +0800

    Resolve conflicts

--- core/init-theme.el
+++ core/init-theme.el
@@ -1,10 +1,6 @@
 ;;; -*- lexical-binding: t -*-
 
 (straight-use-package 'kaolin-themes)
-<<<<<<< HEAD
-(straight-use-package 'emacs-neotree)
-=======
->>>>>>> dedbe5d (found back my lost sidebar!)
 (straight-use-package 'dired-sidebar)
 (straight-use-package
  '(zenburn-emacs :type git :host github :repo "bbatsov/zenburn-emacs"))
@@ -36,7 +32,6 @@
     (require 'kaolin-light-tweak-theme)
     (load-theme 'kaolin-light-tweak t)))
 
-<<<<<<< HEAD
 ;; font
 (when (display-graphic-p)
   (tool-bar-mode -1)
@@ -45,29 +40,19 @@
   (setq default-frame-alist '((width . 169) (height . 48)))
   (cond (*is-mac*
          (add-to-list 'default-frame-alist
-<<<<<<< HEAD
-                      '(font . "Inconsolata-16")))
-=======
-                      '(font . "IBM Plex Mono-16")))
->>>>>>> 432bc88 (clean working tree)
         (*is-linux*
          (add-to-list 'default-frame-alist
                       ;'(font . "Courier New-16:bold")))))
-                      '(font . "IBM Plex Mono-16")))))
+                      '(font . "IBM Plex Mono-16")))))))
 ;; use terminal theme/font in TUI with minor fixes
 (when (not (display-graphic-p))
   (menu-bar-mode -1))
 
-=======
->>>>>>> 0007395 (use LXGW Wenkai for Chinese characters)
 ;; tree
 (autoload
   #'dired-sidebar-toggle-sidebar "dired-sidebar" nil t)
-<<<<<<< HEAD
-(global-set-key (kbd "C-v") #'dired-sidebar-toggle-sidebar)
-=======
 (global-set-key (kbd "C-c f") #'dired-sidebar-toggle-sidebar)
->>>>>>> dedbe5d (found back my lost sidebar!)
+
 
 (with-eval-after-load "dired-sidebar"
   (add-hook 'dired-sidebar-mode-hook 'hl-line-mode)
