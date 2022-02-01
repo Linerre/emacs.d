7a151db34686cdac3d49d5cdcf52790593f9b696
Linerre <leon.errelin@outlook.com>
2022-02-01 17:47:23 +0800

    Use the same font on macOS and Linux
    Note: on macOS the GUI initial frame width is
    kept unchanged. On Linux this is handled by wm

--- core/init-font.el
+++ core/init-font.el
@@ -2,16 +2,13 @@
 
 (defvar +chinese-font-family "LXGW WenKai")
 
+;; GUI
 (when (display-graphic-p)
-  (cond (*is-mac*
-         (setq default-frame-alist '((width . 120) (height . 48)))
-         (add-to-list 'default-frame-alist '(font . "Roboto Mono-18"))
-         ;; use LXGW for chinese characters
-         (set-fontset-font t 'han (font-spec :family +chinese-font-family)))
-
-        (*is-linux*
-         (add-to-list 'default-frame-alist
-                      '(font . "IBM Plex Mono-16")))))
+  (when *is-mac*
+    (setq default-frame-alist '((width . 120) (height . 48))))
+  (add-to-list 'default-frame-alist '(font . "Roboto Mono-18"))
+  ;; use LXGW for chinese characters
+  (set-fontset-font t 'han (font-spec :family +chinese-font-family)))
 
 ;; TUI
 (when (not (display-graphic-p))
