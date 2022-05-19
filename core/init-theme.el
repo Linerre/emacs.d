d135e237fb9db57541bcba092d026680164ecf07
Linerre <leon.errelin@outlook.com>
2022-05-20 01:48:19 +0800

    Restore dired-sidebar (file tree)
    I dropped it at 22446ddf6b170978c91d4a684e564971cb0ca245,
    but while skimming through files in a project using
    Sublime Text, I realized sidebar is quite useful for
    click-to-view files in GUI.

--- core/init-theme.el
+++ core/init-theme.el
@@ -21,18 +21,29 @@
     (progn
       (tool-bar-mode -1)
       (scroll-bar-mode -1)
-      ;; (menu-bar-mode -1)
       (require 'modus-themes)
       (setq modus-themes-italic-constructs t
             modus-themes-bold-constructs nil
             modus-themes-region '(bg-only no-extend))
       (load-theme 'modus-operandi t))
   (progn
+    (menu-bar-mode -1)                  ; almost useless in TUI
     (require 'kaolin-themes)
     (load-theme 'kaolin-light t)
     (require 'kaolin-light-tweak-theme)
     (load-theme 'kaolin-light-tweak t)))
 
+;; tree sidebar is useful when viewing a project
+(sup 'dired-sidebar)
+(setq dired-sidebar-theme 'ascii
+      dired-sidebar-width 30
+      dired-sidebar-use-custome-font t
+      dired-sidebar-face '(:weight bold))
+(autoload
+  #'dired-sidebar-toggle-sidebar "dired-sidebar" nil t)
+(global-set-key (kbd "<f8>") #'dired-sidebar-toggle-sidebar)
+(with-eval-after-load "dired-sidebar"
+  (add-hook 'dired-sidebar-mode-hook 'hl-line-mode))
 
 (provide 'init-theme)
 
