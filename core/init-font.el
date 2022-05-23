585c8f0ead9891c8208872ae719a7276050b09eb
Linerre <leon.errelin@outlook.com>
2022-05-23 23:19:47 +0800

    Use Menlo on macOS and Consolas on Linux
    For quite a while I hate the shape of lowercase "l" in fonts like Menlo
    But over time I changed my mind. These days I focus more on code, not
    really paying too much attention on the font. I want some rounded feeling
    of a font, like Menlo, Source Code Pro or the like.

--- core/init-font.el
+++ core/init-font.el
@@ -5,8 +5,12 @@
 ;; GUI
 (when (display-graphic-p)
   (when *is-mac*
-    (setq default-frame-alist '((width . 120) (height . 48))))
-  (add-to-list 'default-frame-alist '(font . "Roboto Mono-18"))
+    (setq default-frame-alist '((width . 120) (height . 48)))
+    (add-to-list 'default-frame-alist '(font . "Menlo-18")))
+
+  (when *is-linux*
+    (add-to-list 'default-frame-alist '(font . "Consolas-16")))
+
   ;; use LXGW for chinese characters
   (set-fontset-font t 'han (font-spec :family +chinese-font-family)))
 
