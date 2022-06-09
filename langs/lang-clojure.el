dfddf1cfdaea50e083d75d39e98b9db1f1a82ed2
Linerre <leon.errelin@outlook.com>
2022-06-10 02:51:41 +0800

    Enable meow and eletric-pair-mode in cider-repl
    the pair mode should really be hooked to cider-repl after cider-repl loaded

--- langs/lang-clojure.el
+++ langs/lang-clojure.el
@@ -34,11 +34,14 @@
         cider-show-error-buffer 'except-in-repl
         cider-print-fn 'zprint)
   (cider-add-to-alist 'cider-jack-in-dependencies
-                      "zprint/zprint" "1.2.3")
-  (add-hook 'cider-repl-mode-hook 'electric-pair-mode)
-  ;; Tab doesn't complete instead it moves cursor as C-a was pressed
-  ;; Use the default C-a/w/d to move to left/right/up windows
-  (add-hook 'cider-repl-mode-hook (lambda () (meow-mode -1))))
+                      "zprint/zprint" "1.2.3"))
+
+(with-eval-after-load "cider-repl"
+    (add-hook 'cider-repl-mode-hook 'electric-pair-mode)
+    ;; Tab doesn't complete instead it moves cursor as C-a was pressed
+    ;; Use the default C-a/w/d to move to left/right/up windows
+    ;; (add-hook 'cider-repl-mode-hook (lambda () (meow-mode -1)))
+)
 
 (provide 'lang-clojure)
 ;;; lang-clojure.el ends here
