8e57beea238e60f6e332634463e985d761086d7d
Linerre <leon.errelin@outlook.com>
2022-08-08 01:11:59 +0800

    Fix syntax

--- core/init-paredit.el
+++ core/init-paredit.el
@@ -4,7 +4,7 @@
 
 (sup 'paredit)
 
-(autoload #'paredit-mode "smartparens" nil t)
+(autoload #'paredit-mode "paredit" nil t)
 
 ;; (with-eval-after-load "paredit"
 ;;   ;; hooks, keybinds, setq etc
@@ -12,18 +12,23 @@
 
 (dolist (h '(c-mode-hook
              c++-mode-hook
-             cider-repl-mode-hook
              css-mode-hook
-             emacs-lisp-mode-hook
              go-mode-hook
              jinja2-mode-hook
              js-mode-hook
-             lisp-mode-hook
-             lisp-interaction-mode-hook
              nix-mode-hook
              org-mode-hook
              python-mode-hook
              sql-mode-hook))
+  (add-hook h #'electric-pair-mode))
+
+(dolist (h '(c-mode-hook
+             c++-mode-hook
+             cider-repl-mode-hook
+             css-mode-hook
+             emacs-lisp-mode-hook
+             lisp-mode-hook
+             lisp-interaction-mode-hook))
   (add-hook h #'paredit-mode))
 
-(provide init-paredit)
+(provide 'init-paredit)
