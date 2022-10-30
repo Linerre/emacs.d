1e60ae57cdcd8ca39e3601a2d7134ef1bed735a9
Linerre <leon.errelin@outlook.com>
2022-10-30 15:50:47 +0800

    Set tab-width to 4 in go mode

--- langs/lang-go.el
+++ langs/lang-go.el
@@ -3,6 +3,7 @@
  '(go-mode.el :type git :host github :repo "dominikh/go-mode.el"))
 
 (autoload #'go-mode "go-mode" nil t)
+(add-hook 'go-mode-hook (lambda () (setq tab-width 4)))
 
 (provide 'lang-go)
 ;;; lang-go.el ends here
