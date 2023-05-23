0c65268e747fa4861167348db8a7b6583301d44b
Linerre <leon.errelin@outlook.com>
2023-05-23 21:34:31 +0800

    Move setq out of eval scope

--- core/init-linter.el
+++ core/init-linter.el
@@ -7,8 +7,8 @@
 (autoload #'flymake-mode "flymake" nil t)
 (global-set-key (kbd "C-c C-f m") #'flymake-mode)
 
+(setq flymake-no-changes-timeout 2)
 (with-eval-after-load "flymake"
-  (setq flymake-no-changes-timeout 2)
   (define-key flymake-mode-map (kbd "C-c k") 'flymake-show-diagnostics-buffer)
   (define-key flymake-mode-map (kbd "M-n") 'flymake-goto-next-error)
   (define-key flymake-mode-map (kbd "M-p") 'flymake-goto-prev-error))
