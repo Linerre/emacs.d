da1e9c52e0d8e651ace073cc4cecb1036a92f4d2
Linerre <leon.errelin@outlook.com>
2023-06-12 00:03:29 +0800

    Cache custom file

--- custom.el
+++ custom.el
@@ -3,8 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(cdlatex which-key smartparens selectrum org-roam ob-restclient nix-mode meow markdown-mode magit latex-preview-pane kaolin-themes json-mode js2-mode jinja2-mode highlight-defined flycheck-clj-kondo exec-path-from-shell emmet-mode eglot company clj-refactor auctex-lua auctex-latexmk)))
+ '(custom-safe-themes
+   '("b7aa4f3bd515d1b4c266e8f3824d2c1feed84b241e2b1581bc47965087a31c84" default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
