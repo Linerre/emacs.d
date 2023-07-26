1f5eb5e3fd9e0693dfbda094ade1440aa1a86e11
Linerre <leon.errelin@outlook.com>
2023-07-26 23:32:40 +1000

    Use IBM VGA8
    See the below source:
    https://www.reddit.com/r/emacs/comments/g4z37e/oldschool_cool_ibm_vga8_font/

--- core/init-theme.el
+++ core/init-theme.el
@@ -7,15 +7,16 @@
 
 ;; For Monospace use 16
 ;; (add-to-list 'default-frame-alist '(font . "Px437 IBM EGA 9x14-16"))
-(add-to-list 'default-frame-alist '(font . "Ac437 IBM VGA 8x16-16"))
+(add-to-list 'default-frame-alist '(font . "Px437 IBM VGA 8x16-16"))
 (set-fontset-font t 'han (font-spec :family +chinese-font-family))
 (set-face-attribute 'variable-pitch nil :family "Sans Serif" :font "Crimson-24")
-(set-face-attribute 'fixed-pitch nil :font "Ac437 IBM VGA 8x16-16")
+(set-face-attribute 'fixed-pitch nil :font "Px437 IBM VGA 8x16")
 
 ;; Theme
 (defvar my-themes
   '(alabaster
     carbon
+    console
     ft))
 
 (defun +toggle-themes ()
