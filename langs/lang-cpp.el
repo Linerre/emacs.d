ec4ebc38e0467d8e5521ad7e46cb41f1226775ce
Linerre <leon.errelin@outlook.com>
2024-04-23 22:16:49 +1000

    Use different indentation in c and java

--- langs/lang-cpp.el
+++ langs/lang-cpp.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 ;; Commentary:
-;; Config for c and cpp
+;; Config for c-like languages such as c and cpp (and java)
 
 ;; Code:
 ;; Inspired by 'compile-command doc
@@ -32,7 +32,18 @@
 
 (add-hook 'c++-mode-hook (lambda ()
                            (modify-syntax-entry ?> "." c++-mode-syntax-table)
-                           (modify-syntax-entry ?< "." c++-mode-syntax-table)))
+                           (modify-syntax-entry ?< "." c++-mode-syntax-table)
+                           (setq-local c-basic-offset 2
+                                       tab-width 2
+                                       indent-tabs-mode t)))
+
+(add-hook 'java-mode-hook (lambda ()
+                            ;; (setq c-default-style "java")
+                            (setq-local c-basic-offset 2)
+                            (setq-local tab-width 2)
+                            (c-set-offset 'arglist-intro '+)
+                            (c-set-offset 'arglist-close '0)
+                            (c-set-offset 'case-label '+)))
 
 (with-eval-after-load "cc-mode"
   (define-key c-mode-base-map [f9] #'+code-compile)
