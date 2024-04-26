6fd1ba3f3a1cd27c79de9c3a4bea767b589f4d19
Linerre <leon.errelin@outlook.com>
2024-04-26 22:18:36 +1000

    Use Guile as the default Scheme

--- langs/lang-lisp.el
+++ langs/lang-lisp.el
@@ -19,10 +19,13 @@
   (add-hook 'lisp-interaction-mode-hook #'eldoc-mode))
 
 ;; scheme
-(setq scheme-program-name "mit-scheme")
+;; (setq scheme-program-name "mit-scheme")
+;; (setq scheme-program-name "csi -:c")
+(setq scheme-program-name "guile")
 (with-eval-after-load "scheme-mode"
   (add-hook 'scheme-mode-hook #'paredit-mode)
-  (add-hook 'scheme-mode-hook #'geiser-mode))
+  (add-hook 'scheme-mode-hook #'geiser-mode)
+  (define-key scheme-mode-map (kbd "C-c g") #'geiser))
 
 (with-eval-after-load "geiser-repl"
   (add-hook 'geiser-repl-mode-hook #'paredit-mode))
