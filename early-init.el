52f622c77fc8228cab6ac237869c3778590b724f
Linerre <leon.errelin@outlook.com>
2024-10-24 15:49:48 +1100

    Move some configs to early init

--- early-init.el
+++ early-init.el
@@ -12,6 +12,33 @@
 (add-to-list 'load-path (expand-file-name "langs" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "core" user-emacs-directory))
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; Projects root
+(defconst *is-win* (string-equal system-type "windows-nt"))
+(defconst *is-linux* (string-equal system-type "gnu/linux"))
+(defconst projects-root "~/projects/")
+(setq custom-theme-directory "~/.emacs.d/themes/")
+
+;; By default Emacs (Makefile) adds this path to `load-path' and puts
+;; a `subdirs.el' there to load all the sub-directories.  Yet on
+;; Arch/Gentoo Emacs site-lisp points to `usr/share/emacs'.  Merely
+;; adding the ``local'' version to `load-path' does not work!
+;; Instead, treat it as any arbituary directory and this trick must be
+;; used. For more see:
+;; [1] https://lists.nongnu.org/archive/html/emacs-devel/2003-04/msg00315.html
+;; [2] https://www.gnu.org/software/emacs/manual/html_node/elisp/Library-Search.html
+;; [3] https://www.emacswiki.org/emacs/LoadPath
+(let ((default-directory  "/usr/local/share/emacs/site-lisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;;; envs
+(setenv "LSP_USE_PLISTS" "true")
+
+;;; disable modes early
+(blink-cursor-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(window-divider-mode 0)
+;; (menu-bar-mode -1)
 
 (require 'init-straight)
 (require 'init-options)
